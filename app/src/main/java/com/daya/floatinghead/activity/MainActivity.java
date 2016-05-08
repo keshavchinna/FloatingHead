ff22182dfb680d7dd5bf03443b39d6b3a42d3cc1
Keshav Yamalapati <keshav@eggheadcreative.com>
2016-05-08 20:27:41 +0530

    added onclick on floating head and showing popup window

--- app/src/main/java/com/daya/floatinghead/activity/MainActivity.java
+++ app/src/main/java/com/daya/floatinghead/activity/MainActivity.java
@@ -1,10 +1,13 @@
-package com.daya.floatinghead;
+package com.daya.floatinghead.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.daya.floatinghead.R;
+import com.daya.floatinghead.Service.FloatingHeadService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
       @Override
       public void onClick(View v) {
-        startService(new Intent(getApplication(), ChatHeadService.class));
+        startService(new Intent(getApplication(), FloatingHeadService.class));
 
       }
     });
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
       @Override
       public void onClick(View v) {
-        stopService(new Intent(getApplication(), ChatHeadService.class));
+        stopService(new Intent(getApplication(), FloatingHeadService.class));
 
       }
     });
